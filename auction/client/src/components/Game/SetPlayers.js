d24de48787c60cd9a424d1a00419e9e8521908d1
zglau <97938344+zglau@users.noreply.github.com>
2022-01-19 17:59:17 +0800

    Add files via upload

---
+++ auction/client/src/components/Game/SetPlayers.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+export const SetPlayers = props => {
+  const {
+    numOfPlayers,
+    invalidNumOfPlayers,
+    updateNumberOfPlayers,
+    showLayout
+  } = props;
+  return (
+    <div>
+      <p>Start game by selecting number of players (Max 4 are allowed)</p>
+      <div>
+        <input
+          type="number"
+          onChange={updateNumberOfPlayers}
+          value={numOfPlayers}
+          disabled
+          max="4"
+          min="2"
+        />
+        {invalidNumOfPlayers && (
+          <p className="error">Please select a number between 2 and 4</p>
+        )}
+        <button
+          className="m-l-20"
+          disabled={!numOfPlayers || numOfPlayers < 2 || numOfPlayers > 4}
+          onClick={showLayout}
+        >
+          {" "}
+          START
+        </button>
+      </div>
+    </div>
+  );
+};
